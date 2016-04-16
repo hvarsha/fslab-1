829d3a67889630ca603ff8ac953b01376f11fe9a
hvarsha <hvarsh95@gmail.com>
2016-04-16 09:08:30 +0530

    Update lab12.cpp

--- lab12/src/lab12.cpp
+++ lab12/src/lab12.cpp
@@ -5,162 +5,238 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
-#include<stdio.h>
-#include<stdlib.h>
+#include<sys/types.h>
 #include<string.h>
 #include<fstream>
-#include<iostream>
-#include<iomanip>
+#include<sstream>
+#include <iostream>
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#define max 100
 using namespace std;
-class node
-{
-	public:	char name[15];
-		char usn[15];
-		node * link;
-};
-
-node * first=NULL;
-
-void writefile()
-{
-	node * p;
-	char buffer[100];
-	fstream out;
-	out.open("student.txt",ios::app);
-	if(!out)
+class Student {
+	string name;
+	string usn;
+	string branch;
+	string sem;
+public:
+	Student(string iname,string iusn,string ibranch,string isem)
 	{
-		cout<<"unable to open the file in append mode";
-		
-		exit(0);
+		name=iname;
+		usn=iusn;
+		branch=ibranch;
+		sem=isem;
 	}
-	p=first;
-	while(p!=NULL)
+	string pack(int n)
 	{
-		strcpy(buffer,p->name);
-		strcat(buffer,"|");
-		strcat(buffer,p->usn);
-		strcat(buffer,"\n");
-		out<<buffer;
-		p=p->link;
-	}
-}
-
-void display()
-{
-	node * p;
-	if(first==NULL)
-	{
-		cout<<"list is empty";
-		return;
-	}
-	p=first;
-	while(p!=NULL)
-	{
-		cout<<"|"<<p->name<<" "<<p->usn<<"|"<<"->";
-		p=p->link;
-	}
-}
-
-void insert()
-{
-	char name[20];
-	char usn[15];
-	node *p;
-	node *q;
-
-	cout<<"\nenter the name\n";
-	cin>>name;
-	cout<<"\nenter the usn\n";
-	cin>>usn;
-	cout<<"\n\n";
-	
-	p=new node;
-	strcpy(p->name,name);
-	strcpy(p->usn,usn);
-	p->link=NULL;
-
-	if(first==NULL)
-	{
-		first=p;
-		writefile();
-		display();
-		return;
-	}
-	for(q=first;q->link!=NULL;q=q->link)
-	{
-		;
-	}
-	q->link=p;
-	writefile();
-	display();
-}
-
-void del()
-{
-	char usn[15];
-	node * curr;
-	node * prev;
-	node * del;
-	if(first==NULL)
-	{
-		printf("the list is empty.. deletion not possible");
-		return;
-	}
-	cout<<"enter the usn to be deleted = ";
-	cin>>usn;
-	if(strcmp(first->usn,usn)==0)
-	{
-		cout<<"record deleted";
-		del=first;
-		delete(del);
-		first=first->link;
-		writefile();
-		return;
-	}
-	prev=NULL;
-	curr=first;
-	while((strcmp(curr->usn,usn)!=0) && curr!=NULL)
-	{
-		prev=curr;
-		curr=curr->link;
-	}
-	if(curr==NULL)
-	{
-		cout<<"the student with usn"<<usn<<"is not present";
-		return;
-	}
-	prev->link=curr->link;
-	writefile();
-	display();
-}
-
-int main()
-{
-	int choice;
-	
-	while(1)
-	{
-		printf("\n\n1 : insert_rear");
-		printf("\n2 : delete_id");
-		printf("\n3 : exit");
-
-
-		printf("\n\nenter the choice\n");
-		scanf("%d",&choice);
-		switch(choice)
+		string buffer;
+		buffer=usn+'|'+name+'|'+branch+'|'+sem;
+		for(int i=buffer.size();i<n;i++)
 		{
-			case 1:	insert();
-				break;
-
-			case 2:	del();
-				break;
-
-			case 3:	exit(0);
-	
-			default:	cout<<"invalid option";
-				break;
+					buffer+='$';
+		}
+		return buffer;
+	}
+};
+class Node
+{
+	int offset;
+	Node *next;
+public:
+	Node()
+	{
+		next=NULL;
+	}
+	Node(int ioffset)
+	{
+		offset=ioffset;
+	}
+	friend class List;
+};
+class List
+{
+	Node *head;
+public:
+	List()
+	{
+		head=NULL;
+	}
+	void insert(int offset)
+	{
+		Node *temp=new Node(offset);
+		temp->next=head;
+		head=temp;
+	}
+	int get()
+	{
+		if(head==NULL)
+		{
+			return -1;
+		}
+		int offset=head->offset;
+		head=head->next;
+		return offset;
+	}
+};
+class RBF
+{
+	char filename[max];
+	int recsize;
+public:
+	RBF(char ifilename[max],int recsiz)
+	{
+		strcpy(filename,ifilename);
+		recsize=recsiz;
+	}
+	void addrec(string buffer,int offset)
+	{
+		ofstream f;
+		if(offset==-1)
+		{
+			fstream f(filename,ios::out|ios::app);
+			f<<buffer;
+			f<<endl;
+			f.close();
+		}
+		else
+		{
+		fstream f(filename);
+		cout<<"value of offset : "<<offset<<endl;
+		f.seekp(offset,ios::beg);
+		f<<buffer;
+		f<<endl;
+		f.close();
 		}
 	}
-	return 0;
-}	 
+	int dele(string key,string value)
+	{
+		cout<<"inside delete"<<endl;
+		string name;
+		string a="*";
+		string usn;
+		string branch;
+		string sem;
+		int i=0,pos=0 ;
+		string s;
+		stringstream ss;
+		cout<<"filename : "<<filename<<endl;
+		fstream f(filename,ios::in);
+		while(!f.eof())
+		{
+			getline(f,s);
+			cout<<"s : "<<s<<endl;
+			ss.str("");
+			ss<<s;
+			pos=f.tellp();
+			pos=pos-101;
+			getline(ss,usn,'|');
+			cout<<"usn : "<<usn<<endl;
+			if(key=="usn")
+			{
+				if(value==usn)
+				{
+					fstream of(filename);
+					of.clear();
+					of.seekp(pos,ios::beg);
+					of<<'*';
+					int apos=of.tellp();
+					cout<<"value of apos : "<<apos<<endl;
+					of.close();
+					return pos;
+				}
+			}
+			getline(ss,name,'|');
+			if(key=="name")
+				{
+					if(value==name)
+					{
+						f.clear();
+						f.seekp(pos,ios::beg);
+						f<<'*';
+						return pos;
+					}
+				}
+			getline(ss,branch,'|');
+			if(key=="branch")
+				{
+					if(value==branch)
+					{
+						f.clear();
+						f.seekp(pos,ios::beg);
+						f<<'*';
+						return pos;
+					}
+				}
+			getline(ss,sem,'$');
+			if(key=="sem")
+				{
+					if(value==sem)
+					{
+						f.clear();
+						f.seekp(pos,ios::beg);
+						f<<'*';
+						return pos;
+					}
+				}
+		}
+	}
+};
+int main()
+{
+	List l1;
+	RBF r1("data.txt",100);
+	string s;
+	int pos;
+	int i;
+	Student s1("abc","678","ise","7");
+	Student s2("def","123","cse","7");
+	Student s3("ghi","456","tce","7");
+	Student s4("jkl","789","me","7");
+	s=s1.pack(100);
+	pos=l1.get();
+	r1.addrec(s,pos);
+	s=s2.pack(100);
+	pos=l1.get();
+	r1.addrec(s,pos);
+	s=s3.pack(100);
+	pos=l1.get();
+	r1.addrec(s,pos);
+	s=s4.pack(100);
+	pos=l1.get();
+	r1.addrec(s,pos);
+	cout<<"Do you want to delete\npress 1 if yes else 0 : ";
+	cin>>i;
+	cout<<endl;
+	if(i==1)
+	{
+		pos=r1.dele("usn","123");
+		cout<<"value of pos : "<<pos<<endl;
+		l1.insert(pos);
+		pos=r1.dele("usn","456");
+		cout<<"value of pos : "<<pos<<endl;
+		l1.insert(pos);
+		cout<<"i guess u deleted succesfully"<<endl;
+	}
+	cout<<"Do you want to add a record ?\npress 1 if yes else 0 : ";
+	cin>>i;
+	cout<<endl;
+	if(i==1)
+	{
+		pos=l1.get();
+		cout<<"value of pos via get() : "<<pos<<endl;
+		Student s5("alpha","159","ise","7");
+		s=s5.pack(100);
+		r1.addrec(s,pos);
+		pos=l1.get();
+		cout<<"value of pos via get() : "<<pos<<endl;
+		Student s6("beta","753","ise","7");
+		s=s6.pack(100);
+		r1.addrec(s,pos);
+		pos=l1.get();
+		cout<<"value of pos via get() : "<<pos<<endl;
+		Student s7("gamma","842","ise","7");
+		s=s7.pack(100);
+		r1.addrec(s,pos);
+	}
+}
